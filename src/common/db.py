a470ec0fe6c1cb5f2489fee8687766a860823d87
xiaomoe <studyxiao@163.com>
2023-03-17 11:01:01 +0800

    feat(api): 增加数据库连接功能

---
+++ src/common/db.py
@@ -0,0 +1,59 @@
+from contextvars import ContextVar
+from typing import cast
+
+from flask import Flask
+from sqlmodel import Session, create_engine
+from werkzeug.local import LocalProxy
+
+from src.config import config
+
+ctx_session: ContextVar[Session] = ContextVar("session")
+
+
+class DB:
+    def __init__(self, app: Flask | None = None) -> None:
+
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask) -> None:
+        self.app = app
+        url: str = config.SQLALCHEMY_DATABASE_URI
+        pool_size: int = config.SQLALCHEMY_POOL_SIZE
+        app.config.setdefault("SQLALCHEMY_TRACK_MODIFICATIONS", False)
+
+        app.before_request(self.before_request)
+        app.teardown_request(self.teardown_request)
+
+        connect_args = {"check_same_thread": False} if "sqlite" in url else {}
+
+        self.engine = create_engine(
+            url,
+            connect_args=connect_args,
+            pool_size=pool_size,
+            pool_recycle=7200,
+        )
+        app.extensions["sqlalchemy"] = self
+
+    def connect(self) -> Session:
+        """生成新的 session."""
+        # session 并不代表连接 只有 exec 时才会真正连接数据库
+        # session 可以看作是本地缓存
+        return Session(self.engine)
+
+    def teardown_request(self, exception: BaseException | None) -> None:
+        try:
+            session = ctx_session.get()
+            session.close()
+            ctx_session.reset(self.token)
+        except LookupError:
+            pass
+
+    def before_request(self) -> None:
+        # 每次 request 创建新的 session 确保事务正确
+        self.token = ctx_session.set(self.connect())
+
+
+db = DB()
+
+session = cast(Session, LocalProxy(ctx_session))
